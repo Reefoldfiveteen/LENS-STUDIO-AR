4c93a8b81eccd55350dbdefa30dcb4275d8b432b
Arif Purnomo Aji <Arifpurnomoaji998@gmail.com>
2022-07-17 22:12:58 +0700

    Add files via upload

---
+++ Tugas Proyek Mandiri/QUIZ_MERDEKA/QUIZ_MERDEKA/Public/Pertanyaan.js
@@ -0,0 +1,90 @@
+// -----JS CODE-----
+
+
+const myQuestions = [
+  {
+    question: "Pada tanggal berapakah Kemerdekaan Republik Indonesia?",
+    answers: {
+      a: "15 Agustus 1945",
+      b: "17 Agustus 1945",
+      c: "15 Agustus 1954",
+      d: "17 Agustus 1954"
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: "Dimanakah Proklamasi Kemerdekaan RI dilaksankan?",
+    answers: {
+      a: "Jalan Pegangsaaan Timur No. 56",
+      b: "Rumah laksamana Maeda",
+      c: "Rengasdengklok",
+      d: "Istana merdeka"
+    },
+    correctAnswer: "a"
+  },
+  {
+    question: "Siapakah yang membuat dan membacakan teks Proklamasi?",
+    answers: {
+      a: "Ir. Soekarno",
+      b: "Drs. Moh. Hatta",
+      c: "Ir. Soekarno dan Drs. Moh. Hatta",
+      d: "Mr. Soepomo"
+
+    },
+    correctAnswer: "a"
+  },
+  {
+    question: "Siapakah yang menjahit bendera Sang Saka Merah Putih?",
+    answers: {
+      a: "R.A Kartini",
+      b: "Cut Nyak Dien",
+      c: "S.K Trimukti",
+	  d: "Fatmawati",
+    },
+    correctAnswer: "d"
+  },
+    {
+    question: "Siapakah Pencipta Lagu Indonesia Raya?",
+    answers: {
+      a: "Sutan Syahrir",
+      b: "A. T Mahmud",
+      c: "Wage Rudolf Supratman",
+      d: "Sarbini"
+    },
+    correctAnswer: "c"
+  },
+    {
+    question: "Siapakah yang mengetik ulang teks proklamasi?",
+    answers: {
+      a: "Ir. Soekarno",
+      b: "Sayuti Melik",
+      c: "Prof. Mr. Mohammad Yamin, S.H.",
+      d: "Mr. Soepomo"
+    },
+    correctAnswer: "b"
+  },
+    {
+    question: "Dimanakah Ir. Soekarno dan Drs. Moh. Hatta diamankah oleh golongan muda?",
+    answers: {
+      a: "Jalan Pegangsaaan Timur No. 56",
+      b: "Rumah laksamana Maeda",
+      c: "Rengasdengklok",
+      d: "Istana merdeka"
+    },
+    correctAnswer: "c"
+  },
+    {
+    question: "Negara manakah yang pertama kali mengakui kemerdekaan indonesia?",
+    answers: {
+      a: "Mesir",
+      b: "Irak",
+      c: "Inggris",
+      d: "India"
+    },
+    correctAnswer: "a"
+  },
+  
+];
+
+//script.api.title = title
+script.api.myQuestions = myQuestions
---
+++ Tugas Proyek Mandiri/QUIZ_MERDEKA/QUIZ_MERDEKA_INSHIGHT/README.md
@@ -0,0 +1,10 @@
+## Pake Peci Screenshoot Insight
+
+#### Insight
+![Screenshot](https://raw.githubusercontent.com/Reefoldfiveteen/LENS-STUDIO-AR/main/Tugas%20Proyek%20Mandiri/pake%20peci/Pake%20Peci%20Lens%20Insight/Insight.png)
+
+
+## Authors
+
+- [@Reefoldfiveteen/Arif Purnomo Aji](https://github.com/Reefoldfiveteen)
+
