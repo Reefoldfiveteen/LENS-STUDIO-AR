b395bbaa2099b4d8f4d4dffd9e854df735819d84
Arif Purnomo Aji <Arifpurnomoaji998@gmail.com>
2022-09-23 13:41:00 +0700

    Add files via upload

---
+++ Tugas Proyek Mandiri/Quiz_Tahu_Batik?/Quiz Tahu Batik/Public/Pertanyaan.js
@@ -0,0 +1,99 @@
+// -----JS CODE-----
+
+
+const myQuestions = [
+  {
+    question: "Tahun berapa UNESCO meresmikan batik sebagai warisan budaya Indonesia?",
+    answers: {
+      a: "2008",
+      b: "2009",
+      c: "2015",
+      d: "2016"
+    },
+    correctAnswer: "b"
+  },
+  
+  {
+    question: "Hari batik nasional diadakan setiap kapan?",
+    answers: {
+      a: "Setiap 2 Oktober",
+      b: "Setiap hari Kamis",
+      c: "Setiap 28 November",
+      d: "Setiap 1 Oktober"
+    },
+    correctAnswer: "a"
+  },
+    
+  {
+    question: "Apakah alat menulis yang biasa digunakan untuk membatik?",
+    answers: {
+      a: "Canting",
+      b: "Anting",
+      c: "Penting",
+      d: "Gunting"
+    },
+    correctAnswer: "a"
+  },
+  
+    {
+    question: "apa bahan lilin yang digunakan untuk membatik?",
+    answers: {
+      a: "balam",
+      b: "nilam",
+      c: "sulam",
+      d: "malam"
+    },
+    correctAnswer: "d"
+  },
+  
+  {
+    question: "Sebelum kita membatik, sebaiknya membuat ... batik dengan menggunakan pensil",
+    answers: {
+      a: "warna",
+      b: "lilin",
+      c: "pola",
+      d: "surat"
+
+    },
+    correctAnswer: "c"
+  },
+  
+  {
+    question: "Apakah pewarna yang digunakan untuk Pewarnaan secara sintetis dengan meyoletkan warna pada kain?",
+    answers: {
+      a: "remasol",
+      b: "naptol",
+      c: "teses",
+	  d: "tinta",
+    },
+    correctAnswer: "d"
+  },
+  
+    {
+    question: "Disebut apakah proses penghilangan lilin malam pada batik secara keseluruhan?",
+    answers: {
+      a: "kerokan",
+      b: "pelorodan",
+      c: "remekan",
+      d: "nggirahi"
+    },
+    correctAnswer: "b"
+  },
+  
+    {
+    question: "Kota mana yang terkenal sebagai kota batik?",
+    answers: {
+      a: "Bogor",
+      b: "Jepara",
+      c: "Pekalongan",
+      d: "Tangerang"
+    },
+    correctAnswer: "c"
+  },
+    
+
+  
+];
+
+//script.api.title = title
+script.api.myQuestions = myQuestions
